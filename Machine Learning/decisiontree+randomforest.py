0b1eb9edbca3075fdca4bf4695451cc049395d4f
pawutc <56311260+pawutc@users.noreply.github.com>
2020-11-04 17:46:27 +0700

    Create decisiontree+randomforest.py
    Remove Linear Regression and add Random Forest Regression

---
+++ Machine Learning/decisiontree+randomforest.py
@@ -0,0 +1,75 @@
+import numpy as np
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
+plt.style.use('bmh')
+
+df = pd.read_csv(r'C:\Users\NonStopMax\Desktop\profitADXRSI.csv',encoding="utf-16",sep='\t')
+df = df[['Profit']]
+#Create a variable to predict 'x' days out into the future
+future_days = 60
+#Create a new column (the target or dependent variable) shifted 'x' units/days up
+df['Prediction'] = df[['Profit']].shift(-future_days)
+#print the data
+df.tail(4)
+
+X = np.array(df.drop(['Prediction'], 1))[:-future_days]
+print(X)
+y = np.array(df['Prediction'])[:-future_days]
+print(y)
+
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.3)
+
+
+tree = DecisionTreeRegressor().fit(x_train, y_train)
+
+rand = RandomForestRegressor().fit(x_train, y_train)
+
+#Get the feature data, 
+#AKA all the rows from the original data set except the last 'x' days
+x_future = df.drop(['Prediction'], 1)[:-future_days]
+#Get the last 'x' rows
+x_future = x_future.tail(future_days) 
+#Convert the data set into a numpy array
+x_future = np.array(x_future)
+x_future
+
+
+tree_prediction = tree.predict(x_future)
+print( tree_prediction )
+print()
+
+rand_prediction = rand.predict(x_future)
+print(rand_prediction)
+
+#Visualize the data
+predictions = tree_prediction
+#Plot the data
+valid =  df[X.shape[0]:]
+valid['Predictions'] = predictions #Create a new column called 'Predictions' that will hold the predicted prices
+plt.figure(figsize=(16,8))
+plt.title('Model')
+plt.xlabel('Days',fontsize=18)
+plt.ylabel('Profit',fontsize=18)
+plt.plot(df['Profit'])
+plt.plot(valid[['Profit','Predictions']])
+plt.legend(['Train', 'Val', 'Prediction' ], loc='lower right')
+plt.show()
+print("DecisionTreeScore =", tree.score(X,y))
+
+#Visualize the data
+predictions = rand_prediction
+#Plot the data
+valid =  df[X.shape[0]:]
+valid['Predictions'] = predictions #Create a new column called 'Predictions' that will hold the predicted prices
+plt.figure(figsize=(16,8))
+plt.title('Model')
+plt.xlabel('Days',fontsize=18)
+plt.ylabel('Profit',fontsize=18)
+plt.plot(df['Profit'])
+plt.plot(valid[['Profit','Predictions']])
+plt.legend(['Train', 'Val', 'Prediction' ], loc='lower right')
+plt.show()
+print("RandomForestScore =", rand.score(X,y))
